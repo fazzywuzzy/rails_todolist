1c21b787585eee010d7654aba6f46fc19d7f2d55
khairul.amiruf <118145154+fazzywuzzy@users.noreply.github.com>
2024-01-04 16:35:57 +0800

    add update functionality

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,21 +1,40 @@
 class TodosController < ApplicationController
-    def index
-      @todos = Todo.all  
-      @todo = Todo.new 
-    end
-  
-    def create
-      @todo = Todo.create(description: params[:todo][:description])
-      if @todo.valid?
-        redirect_to root_path
-      else
-        render :index
-      end
-    end
-  
-    def destroy
-      @todo = Todo.find(params[:id])
-      @todo.destroy
+  before_action :find_todo, only: %i[destroy update]
+
+  def index
+    @todos = Todo.all
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.create(description: params[:todo][:description])
+    if @todo.valid?
       redirect_to root_path
+    else
+      render :index
     end
   end
+
+  def destroy
+    @todo.destroy
+    redirect_to root_path
+  end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def find_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :todos, only: [:index, :create, :destroy]
+  resources :todos, only: [:index, :create, :destroy, :update] # Add :update to the list of allowed actions
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "todos#index"
-
 end
